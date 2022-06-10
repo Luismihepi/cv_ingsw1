645ef2ce2636d622278a08691f88441fd604f451
57304 <57304@Luis-Miguel-Hernandez>
2022-06-09 23:47:18 -0500

    version 1

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -16,6 +16,7 @@ public class Cv_ejercicio {
      */
     public static void main(String[] args) {
         System.out.println("version 0");
+        System.out.println("version 1");
         // TODO code application logic here
     }
     
